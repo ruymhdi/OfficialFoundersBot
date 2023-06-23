74a9ab7689ca67fd5507e070e0f3cb1c2900d10d
Rasulov Shodiyor <108322250+ruymhdi@users.noreply.github.com>
2023-06-23 17:41:46 +0500

    main

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -12,32 +12,38 @@ async def start_command(message: types.Message, state: FSMContext):
     await bot.send_message(chat_id=message.chat.id, text="Iltimos ism va familiyangizni kiriting.")
     await Lists.name_surname.set()
 
+
 @dp.message_handler(state=Lists.name_surname)
 async def process_name(message: types.Message, state: FSMContext):
     name = message.text
     await state.update_data(name=name)
 
     await bot.send_message(chat_id=message.chat.id, text="Iltimos kontaktingizni yuboring!", reply_markup=types.ReplyKeyboardMarkup(
-            keyboard=[
-                [
-                    types.KeyboardButton(text="📱 Kontaktni yuborish", request_contact=True)
-                ]
-            ],
-            resize_keyboard=True
-        ))
+        keyboard=[
+            [
+                types.KeyboardButton(text="📱 Kontaktni yuborish", request_contact=True)
+            ]
+        ],
+        resize_keyboard=True
+    ))
     await Lists.contact.set()
+
 
 @dp.message_handler(content_types=ContentTypes.CONTACT, state=Lists.contact)
 async def process_contact(message: types.Message, state: FSMContext):
-    data = await state.get_data()
-    # name = data.get('name')
-    # contact = message.contact
+    contact_info = message.contact
+    state_data = await state.get_data()
+    name = state_data['name']
 
-    await bot.send_message(chat_id=message.chat.id, text="Sizning kontaktingiz qabul qilindi.", reply_markup=ReplyKeyboardRemove())
+    await bot.send_message(chat_id=message.chat.id, text="Sizning kontaktingiz qabul qilindi.",
+                           reply_markup=ReplyKeyboardRemove())
 
+    xabar = f"<b>Foydalanuchi ismi</b>: {name}\n" \
+            f"<b>Contact info</b>: {contact_info.phone_number}\n"
 
-    await bot_help(message)
+    await bot.send_message(chat_id='-1001829161444', text=f"{xabar}")
     await state.finish()
+
 
 @dp.message_handler()
 async def handle_all_messages(message: types.Message):
