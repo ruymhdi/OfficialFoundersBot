46a0308cde84ab0dfb263d1f4c05e6bfa1a1f4b0
Rasulov Shodiyor <108322250+xamzaaff@users.noreply.github.com>
2023-12-27 08:20:24 -0500

    Delete handlers/users/utils.py

--- handlers/users/utils.py
+++
@@ -1,6 +0,0 @@
-# from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-#
-#
-# def get_cancel() -> ReplyKeyboardMarkup:
-#     return ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton('/cancel'))
-#
