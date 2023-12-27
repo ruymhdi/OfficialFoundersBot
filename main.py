8335600c9cca48424b4ec5640596a1ca7b5d0924
Rasulov Shodiyor <108322250+xamzaaff@users.noreply.github.com>
2023-12-27 08:24:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ from utils.set_bot_commands import set_default_commands
 
 
 async def on_startup(dispatcher):
-    # Birlamchi komandalar (/star va /help)
     await set_default_commands(dispatcher)
 
-    # Bot ishga tushgani haqida adminga xabar berish
+    # Notify To ADMIN
     await on_startup_notify(dispatcher)
 
 
